7f26d6efa7953cc20ebdd3e727700a7936f25200
paulinakoprowska <paulina.koprowska@opoczta.pl>
2023-05-16 20:25:31 +0200

    Initial Commit

---
+++ src/main/java/pl/sda/orange/java2/carsharing/CarsharingApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.orange.java2.carsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsharingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sda/orange/java2/carsharing/CarsharingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.orange.java2.carsharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
