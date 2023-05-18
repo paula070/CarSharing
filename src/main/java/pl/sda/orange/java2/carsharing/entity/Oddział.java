c47aaa1db336b4d74c7143dc863b61af8793d24e
lucyna <lunda@onet.pl>
2023-05-18 19:15:36 +0200

    [add] - Oddział new methods

--- src/main/java/pl/sda/orange/java2/carsharing/entity/Oddział.java
+++ src/main/java/pl/sda/orange/java2/carsharing/entity/Oddział.java
@@ -1,8 +1,12 @@
 package pl.sda.orange.java2.carsharing.entity;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Oddział {
@@ -12,4 +16,34 @@ public class Oddział {
 
     private String adres;
 
+    private String miasto;
+
+    @OneToMany
+    private List<Pracownik> pracownicy;
+
+    @OneToMany
+    private List<Car> dostepneAuta;
+
+    public Oddział() {
+        pracownicy = new ArrayList<>();
+        dostepneAuta = new ArrayList<>();
+    }
+
+    // Dodaj konstruktor, gettery i settery dla wszystkich pól
+
+    public void dodajPracownika(Pracownik pracownik) {
+        pracownicy.add(pracownik);
+    }
+
+    public void usunPracownika(Pracownik pracownik) {
+        pracownicy.remove(pracownik);
+    }
+
+    public void dodajDostepneAuto(Samochód samochód) {
+        dostepneAuta.add(samochód);
+    }
+
+    public void usunDostepneAuto(Samochód samochód) {
+        dostepneAuta.remove(samochód);
+    }
 }
