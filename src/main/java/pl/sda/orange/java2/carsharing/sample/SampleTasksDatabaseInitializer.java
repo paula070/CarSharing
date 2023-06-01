db0abc24ac4e338c8991a2aeada4bffd3870db36
Aleksandra <aleksandraleszczynska@hotmail.com>
2023-06-01 20:24:39 +0200

    wersja probna - 01.06.23

--- src/main/java/pl/sda/orange/java2/carsharing/sample/SampleTasksDatabaseInitializer.java
+++ src/main/java/pl/sda/orange/java2/carsharing/sample/SampleTasksDatabaseInitializer.java
@@ -17,7 +17,7 @@ import javax.transaction.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class SampleTasksDatabaseInitializer implements CommandLineRunner {
-    private final ToDoService service;
+
 
     private final UserRepository userRepository;
 
@@ -25,17 +25,17 @@ public class SampleTasksDatabaseInitializer implements CommandLineRunner {
     public void run(String... args) throws Exception {
         val user = sampleUser();
 
-        service.add(new CreateTodoDto("user1",  "password1"), user.getId());
-        service.add(new CreateTodoDto("user2",  "password2"), user.getId());
-        service.add(new CreateTodoDto("user3",  "password3"), user.getId());
-        service.add(new CreateTodoDto("user4",  "password4"), user.getId());
-        service.add(new CreateTodoDto("user5",  "password5"), user.getId());
+        //service.add(new CreateTodoDto("user1",  "password1"), user.getId());
+        //service.add(new CreateTodoDto("user2",  "password2"), user.getId());
+        //service.add(new CreateTodoDto("user3",  "password3"), user.getId());
+        //service.add(new CreateTodoDto("user4",  "password4"), user.getId());
+        //service.add(new CreateTodoDto("user5",  "password5"), user.getId());
     }
 
-    private User sampleUser() {
-        val user = new User();
-        user.setLogin("example");
-        return userRepository.saveAndFlush(user);
-    }
+//    private User sampleUser() {
+//        val user = new User();
+//        user.setLogin("example");
+//        return userRepository.saveAndFlush(user);
+//    }
 
 }
